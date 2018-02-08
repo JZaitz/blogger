ca86c378d35de33f744a2a176fc088a201f3f030
Jason Zaitz <jasonzaitz1@gmail.com>
2018-02-08 10:53:43 -0500

    Tagging feature bugs updated

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,18 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
-  end  
+  end
+
+  def index
+    @tag = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    flash.notice = "Tag #{@tag.name} deleted."
+    @tag.articles.destroy
+    redirect_to tag_path
+  end
+
 
 end
