766e5088996817bb7be39da6888da9427bef610f
Jason Zaitz <jasonzaitz1@gmail.com>
2018-02-08 15:32:45 -0500

    Updates for Heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'drinks#index'
 
   root to: 'articles#index'
   resources :articles do
     resources :comments
   end
-  resources :tags 
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
